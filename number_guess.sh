54e4c80a3691a61ba5ed775f98fb98f44924a2c3
Mikey32 <mikeaperry@gmail.com>
2022-06-17 15:15:03 +0000

    fix: Project complete. It needed the entire thing to be in one if block. My tests couldn't find but other tests found an error.

--- number_guess.sh
+++ number_guess.sh
@@ -2,23 +2,22 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 RANDOM_NUMBER=$[ $RANDOM % 1000 + 1 ]
 
-GUESS_COUNT=0
+GUESS_COUNT=1
 GUESS_NUMBER() {
 if [[ ! $1 =~ ^[0-9]+$ ]]
 then
   echo "That is not an integer, guess again:"
   read GUESS
   GUESS_NUMBER $GUESS
-fi
-if [[ $1 -lt $RANDOM_NUMBER ]]
+elif [[ $1 -lt $RANDOM_NUMBER ]]
 then
-  echo "It's higher than that guess again:"
+  echo "It's higher than that, guess again:"
   ((GUESS_COUNT=GUESS_COUNT+1))
   read GUESS
   GUESS_NUMBER $GUESS
 elif [[ $1 -gt $RANDOM_NUMBER ]]
 then
-  echo "It's lower than that guess again:"
+  echo "It's lower than that, guess again:"
   ((GUESS_COUNT=GUESS_COUNT+1))
   read GUESS
   GUESS_NUMBER $GUESS
@@ -30,9 +29,10 @@ else #[[ $1 == $RANDOM_NUMBER ]]
       BEST_GAME_UPDATED=$($PSQL "UPDATE user_info SET best_guess = $GUESS_COUNT WHERE user_id = $USER_ID")
   fi
   ((GAMES_PLAYED=GAMES_PLAYED+1))
-  GAMES_PLAYED_UPDATED=$($PSQL "UPDATE user_info SET games_played = '$GAMES_PLAYED'")
+  GAMES_PLAYED_UPDATED=$($PSQL "UPDATE user_info SET games_played = '$GAMES_PLAYED' WHERE user_id = $USER_ID")
 fi
 }
+
 
 echo "Enter your username:"
 read USER_NAME
@@ -47,7 +47,7 @@ then
 else
   GAMES_PLAYED=$($PSQL "SELECT games_played FROM user_info WHERE user_id = '$USER_ID'")
   BEST_GAME=$($PSQL "SELECT best_guess FROM user_info WHERE user_id = '$USER_ID'")
-  echo "Welcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  echo -e "Welcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.\n"
 fi
 echo "Guess the secret number between 1 and 1000:"
 read GUESS
